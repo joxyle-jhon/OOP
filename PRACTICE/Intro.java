d3d668d7ad06ee8af8d1fdaa5bc447488657c319
Jhon Lloyd Omblero <omblero.jhonlloyd.04@gmail.com>
2024-02-12 08:24:14 +0800

    Initial commit

---
+++ PRACTICE/Intro.java
@@ -0,0 +1,9 @@
+//Java is a popular language
+//Java used to develop mobile apps, web apps, desktop apps, games and much more.
+
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
